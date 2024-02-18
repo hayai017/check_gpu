2e7f6dd741dd2ae3a9a77daecaa3f5f5efcbf459
Hayato Ikenouchi <121442140+hayai017@users.noreply.github.com>
2024-02-19 02:57:29 +0900

    Update check_gpu.py

--- check_gpu.py
+++ check_gpu.py
@@ -1,24 +1,4 @@
-from __future__ import print_function
-import argparse
 import torch
-import torch.utils.data
-from torch import nn, optim
-from torch.nn import functional as F
-from torchvision import datasets, transforms
-from torchvision.utils import save_image
-import os
-
-# バッチサイズ
-batch_size = 32
-# エポック数
-epochs = 100
-# 乱数の種
-seed = 123
-# ログ表示のインターバル
-log_interval = 10
-# 結果出力ディレクトリの作成
-os.makedirs("results", exist_ok=True)
-
 # cuda (GPU)を使用するかどうか(使用可能かどうか)
 # falseになる場合はランタイムのタイプがGPUになっているか確認 
 cuda = torch.cuda.is_available()
